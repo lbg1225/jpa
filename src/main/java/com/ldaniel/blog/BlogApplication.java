8a4b8f936d5392d493040c8b8c7941e6d512f76b
root <root@localhost.localdomain>
2021-02-13 20:38:40 +0900

    환경설정완료 V1

---
+++ src/main/java/com/ldaniel/blog/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.ldaniel.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
